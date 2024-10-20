ccaa13526c206002237316a5cf960af24875f8ae
Martinez817 <26jmartinez@derryfield.org>
2024-10-19 20:55:21 -0400

    Create GitTesting.java

---
+++ AT Java Unit 03 L4 /GitTesting.java
@@ -0,0 +1,14 @@
+/*
+ * 10/19/24
+ * Sw Design with Java 
+ * Jonathan Martinez 
+ * 
+ * This is a test program to see how the GIT repositories work. 
+ */
+
+ public class GitTesting {
+    public static void main(String[] args){
+        System.out.println("Hello World!"); 
+    
+    }
+ }
