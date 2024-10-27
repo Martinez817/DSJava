1619f3b69fd0504b68d6ba7523e36d918380d3c8
Martinez817 <26jmartinez@derryfield.org>
2024-10-27 17:59:26 -0400

    Create Cesium.java

---
+++ AT Java Lesson 8 - Loops/Cesium.java
@@ -0,0 +1,40 @@
+/*
+ * 10/27/24
+ * Sw Design with Java 
+ * Jonathan Martinez 
+ * 
+ * This program is used to calculate how many years will pass until 99% of the original amount of cesium is gone. 
+ */
+import java.util.Scanner; 
+
+public class Cesium {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in); 
+       Double cesiumAmount = 100.0; 
+       int cesiumYear = 1; 
+
+       while (cesiumAmount > 1.0){
+        if (cesiumYear % 30 == 0){
+            cesiumAmount = cesiumAmount / 2.00; 
+            if (cesiumAmount > 1.0){
+                cesiumYear++;
+            } 
+        }
+        else if (cesiumYear % 30 != 0){
+            cesiumYear++; 
+        }
+        
+        if (cesiumAmount <= 1.0){
+            System.out.println("Assuming you start with 100% material, you will have lost around 99% of the original Cesium after " + cesiumYear + " years. Have a good day!");
+            break;
+            
+        } 
+
+       }
+
+       
+       in.close();
+      
+    }
+    
+}
